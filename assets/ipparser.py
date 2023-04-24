9114f8b7e2049f279200ee8bedceaa8d8864cfe2
Sourcery AI <>
2023-04-24 01:35:50 +0000

    'Refactored by Sourcery'

--- assets/ipparser.py
+++ assets/ipparser.py
@@ -2,19 +2,15 @@
 
 import sys
 
-file = open('/usr/local/share/Eternal_Scanner/vuln.txt','r')
-files = open('assets/ip.txt', 'w')
+with open('/usr/local/share/Eternal_Scanner/vuln.txt','r') as file:
+    with open('assets/ip.txt', 'w') as files:
+        # starts from 2nd line
+        file.readline()
+        file.readline()
 
-# starts from 2nd line
-file.readline()
-file.readline()
-
-lines = file.readlines()
-ips = []
-for i in lines:
-    a = i.strip('\n').split(':')[0]
-    ips.append(a)
-    files.write(a+"\n")
-
-files.close()
-file.close()
+        lines = file.readlines()
+        ips = []
+        for i in lines:
+            a = i.strip('\n').split(':')[0]
+            ips.append(a)
+            files.write(a+"\n")
